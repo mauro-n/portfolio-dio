090abd9c20b73357d750a5fa158e0fb97919700b
test <mauro.henrique613@gmail.com>
2023-06-16 11:11:45 -0300

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".itemContainer");
+const items = Array.of(...container.children);
+
+items.forEach(element => {
+    element.querySelector("*").addEventListener("click", () => {
+        if (element.classList.contains("open")) {
+            element.classList.remove("open");
+        } else {
+            element.classList.add("open")
+        }
+    })
+})
